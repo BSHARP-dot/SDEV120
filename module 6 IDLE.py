016337d22f0d5dbc829a9ebb9f8271ace80ad607
Blake Sharp <Bsharp14@ivytech.edu>
2025-04-28 08:59:49 -0400

    Added module 6 IDLE and screenshot

---
+++ module 6 IDLE.py
@@ -0,0 +1,14 @@
+# Program to create the sentence "It is raining cats and dogs"
+
+# sets variable c to "cats"
+c = "cats"
+
+# sets variable d to "dogs"
+
+d = "dogs"
+
+# sets variable s to sentence
+s = "It is raining " + c + " and " + d + "."
+
+# Outputs the sentence
+print(s)
